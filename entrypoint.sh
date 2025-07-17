6039511228d1f773b57147f14c818d827dd95863
SPITZ-Bradley <spitzbradley@gmail.com>
2025-07-17 15:14:02 +0200

    Update entrypoint.sh
    Added map, process and bidsify

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 # ──────────────────────────────────────────────
 # 1) no argument      => interactive shell
 # 2) "prepare"        => bidsme prepare RAW PREP
-# 3) "lab"            => JupyterLab
-# 4) anything else    => passed directly to BIDSme
+# 3) "process"        => bidsme process PREP BIDSIFIED
+# 4) "map"            => bidsme map PREP BIDSIFIED
+# 5) "bidsify"        => bidsme bidsify PREP BIDSIFIED
+# 6) "lab"            => JupyterLab
+# 7) anything else    => passed directly to BIDSme
 # ──────────────────────────────────────────────
 
 if [ "$#" -eq 0 ]; then
@@ -17,6 +20,21 @@ elif [ "$1" = "prepare" ]; then
   shift
   echo "[INFO] Running bidsme prepare with default /mnt paths"
   exec bidsme prepare /mnt/rawdata /mnt/prepared "$@"
+
+elif [ "$1" = "process" ]; then
+  shift
+  echo "[INFO] Running bidsme process with default /mnt paths"
+  exec bidsme process /mnt/prepared /mnt/bidsified "$@"
+
+elif [ "$1" = "map" ]; then
+  shift
+  echo "[INFO] Running bidsme map with default /mnt paths"
+  exec bidsme map /mnt/prepared /mnt/bidsified "$@"
+
+elif [ "$1" = "bidsify" ]; then
+  shift
+  echo "[INFO] Running bidsme bidsify with default /mnt paths"
+  exec bidsme bidsify /mnt/prepared /mnt/bidsified "$@"
 
 elif [ "$1" = "lab" ]; then
   shift
